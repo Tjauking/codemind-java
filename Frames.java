32f86a95b0b1ffa0f50a303ab9f9ec93fa5d22a6
Takudzwa Jauki <121710070+Tjauking@users.noreply.github.com>
2024-07-31 15:33:31 +0530

    import java.util.*; public class Frames{     public static void main(String[] args){         Scanner input=new Scanner(System.in);         int a,b,c;         a=input.nextInt();         b=input.nextInt();         c=input.nextInt();         System.out.print((a+b)*2*c);     } }

---
+++ Frames.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Frames{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        int a,b,c;
+        a=input.nextInt();
+        b=input.nextInt();
+        c=input.nextInt();
+        System.out.print((a+b)*2*c);
+    }
+}
